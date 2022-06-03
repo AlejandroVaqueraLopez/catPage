0e32cc97aba9fa9efd7f9dfb5b9ceb1ed4351a94
Alejandro Vaquera Lopez <alejandrovaqueralopez608@gmail.com>
2022-06-02 20:22:07 -0700

    feat: nav is height adjusted

--- src/views/js/index.js
+++ src/views/js/index.js
@@ -6,9 +6,11 @@ function openMenu(e){
     if(e.type=='click'){
         if(toggle==false){
             menu.style.marginRight = '0px';
+            btnToggleNav.children[0].src = "./icons/menu-icon.png";
         }
         else if(toggle==true){
             menu.style.marginRight = '-500px';
+            btnToggleNav.children[0].src = "./icons/menu-icon-white.png";
         }
         toggle = !toggle;
     }
